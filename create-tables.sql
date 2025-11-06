1fe8264b265ee5c8945432f6bdfe1e9bddaabcc5
Nathan Amidon <legonate@gmail.com>
2025-11-06 12:21:09 -0500

    sql to create and drop tables

---
+++ create-tables.sql
@@ -0,0 +1,61 @@
+CREATE TABLE Users (
+    UserID INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(100),
+    Age INT,
+    Weight INT,
+    bodyFatPercentage DECIMAL(5,2),
+    Gender CHAR(1)
+);
+
+CREATE TABLE Exercise (
+    ExerciseID INT AUTO_INCREMENT PRIMARY KEY,
+    ExerciseName VARCHAR(100) UNIQUE,
+    ExerciseType VARCHAR(100)
+);
+
+CREATE TABLE WorkoutPlan (
+    WorkoutID INT AUTO_INCREMENT PRIMARY KEY,
+    NumOfExercises INT,
+    WorkoutName VARCHAR(100),
+    TargetMuscleGroup VARCHAR(100),
+    DaysOfWeek CHAR(2),
+    ExerciseID INT,
+    FOREIGN KEY (ExerciseID) REFERENCES Exercise(ExerciseID)
+);
+
+CREATE TABLE SupplementLog (
+    WorkoutID INT PRIMARY KEY,
+    ProteinTotalG INT,
+    CreatineTotalG INT,
+    PreworkoutTotalG INT,
+    FOREIGN KEY (WorkoutID) REFERENCES WorkoutPlan(WorkoutID)
+);
+
+CREATE TABLE ExerciseLog (
+    LogID INT AUTO_INCREMENT PRIMARY KEY,
+    NumOfSetsDone INT,
+    NumOfRepsDone INT,
+    Length INT,
+    Weight INT
+);
+
+CREATE TABLE NutritionLog (
+    NutritionDayId INT AUTO_INCREMENT PRIMARY KEY,
+    NumOfCal INT,
+    NumOfCarbs INT,
+    NumOfProtein INT
+);
+
+CREATE TABLE SleepLog (
+    SleepDayID INT AUTO_INCREMENT PRIMARY KEY,
+    SleepTime INT
+);
+
+CREATE TABLE Goal (
+    GoalID INT AUTO_INCREMENT PRIMARY KEY,
+    WeightGoal INT,
+    CalorieGoal INT,
+    ExerciseFrequency INT,
+    SleepGoal INT
+);
+
